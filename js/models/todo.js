1c04079cb67011d3eb0f817ed46faea9e32c23da
macheteshuffle <jeff.li029@gmail.com>
2014-08-13 22:00:40 -0400

    added comments

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,9 +1,25 @@
+/*
+* Model relationships can also be defined. Ember supports 1-1, 1-many and Many-Many relationships
+*/
+
+/* Defines Todo model*/
 Todos.Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean'),
   createdAt: DS.attr('string')
+  /* If you want to declare a belongs to relationships:
+  user: DS.belongsTo('user')
+  */
 });
 
+/* If you had a User model and want to declare has many relationship to Todos:
+Todos.User = DS.Model.extend({
+  todos: DS.hasMany('todo')
+});
+
+*/
+
+/* Used as dummy data but since LocalStorage adapter is used to persist data, this is inactive. */
 Todos.Todo.FIXTURES = [
   {
     id: 1,
